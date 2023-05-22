dbff02993683edc63826b29b89b69299c8ff8575
InancDincer <inanc_01940@hotmail.com>
2023-05-22 11:39:28 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Flutter Animations
+
+This Repo will be concentrating on animation with Flutter!
+
+## Card Animation
+
+https://github.com/SwaroopSambhayya/Flutter-Animations/assets/31922733/82137cec-261d-4037-9df4-645827de6097
+
+# FlutterExamples
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:card_animation/card_animation/card_animation_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+//commit deneme
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+        debugShowCheckedModeBanner: false, home: CardAnimationView());
+  }
+}
